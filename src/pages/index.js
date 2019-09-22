3b367bbe4b23cbf3ecf5b02ed79d60850152599d
Tony Powell <adpowell@mtu.edu>
2019-09-22 13:59:50 -0400

    Start introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,27 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Hello world!</div>
+const Body = styled.div`
+  padding: 50px;
+`
+
+const Intro = styled.h1`
+  font-family: "Rubik";
+  font-size: 3rem;
+  color: #273036;
+`
+
+const Focus = styled.span`
+  font-style: italic;
+  font-size: 3.2rem;
+  color: #4c80b0;
+`
+
+export default () => (
+  <Body>
+    <Intro>
+      Hi! <br /> My name is <Focus>Anthony Powell</Focus>,
+      <br /> but most people call me <Focus>Tony</Focus>.
+    </Intro>
+  </Body>
+)
