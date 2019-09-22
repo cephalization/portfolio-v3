4eac8ca267a4d490128b061e2dc47238124058bf
Tony Powell <adpowell@mtu.edu>
2019-09-22 14:02:21 -0400

    Create Wave svg component

---
+++ src/components/wave/wave.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Wave = props => (
+  <svg viewBox="0 0 1440 320" {...props}>
+    <path
+      fill="#273036"
+      d="M0 32l60 5.3C120 43 240 53 360 58.7c120 5.3 240 5.3 360 48C840 149 960 235 1080 240s240-69 300-106.7l60-37.3V0H0z"
+    />
+  </svg>
+)
