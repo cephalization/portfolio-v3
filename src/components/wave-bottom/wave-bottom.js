2e7f70d1fa74ad44effe0c837ad6a826e0e6b1f6
Tony Powell <adpowell@mtu.edu>
2019-09-22 16:21:35 -0400

    Add svg attr to fix safari rendering issues

--- src/components/wave-bottom/wave-bottom.js
+++ src/components/wave-bottom/wave-bottom.js
@@ -1,7 +1,13 @@
 import React from "react"
 
 export const WaveBottom = props => (
-  <svg viewBox="0 0 1440 320" style={{ display: "block" }} {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    viewBox="0 0 1440 320"
+    style={{ display: "block" }}
+    {...props}
+  >
     <path
       fill="#273036"
       d="M0 96l60 21.3C120 139 240 181 360 192s240-11 360-21.3c120-10.7 240-10.7 360 10.6 120 21.7 240 63.7 300 85.4l60 21.3v32H0z"
