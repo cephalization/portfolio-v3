1057450b2960345d0aadab6d01ed9cf3d453e37d
Tony Powell <adpowell@mtu.edu>
2019-09-22 15:07:56 -0400

    Tweak margins, add animations to intro text

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,32 +12,72 @@ const Body = styled.div`
   }
 `
 
-const Intro = styled.h1`
+const Intro = styled.div`
   font-family: "Rubik";
   font-size: 4rem;
   color: #273036;
-  margin-top: 15rem;
+  margin-top: 10rem;
   margin-left: 10rem;
   @media screen and (max-width: 768px) {
+    font-size: 2.5rem
     margin-top: 5rem;
-    margin-left: 1rem;
+    margin-left: 0;
   }
 `
 
-const Focus = styled.span`
+const Focus = styled.h2`
+  display: inline-block;
+  margin: .4rem 0
   font-style: italic;
   font-size: 5rem;
+  @media screen and (max-width: 768px) {
+    font-size: 3.5rem;
+  }
+`
+
+const Name = styled(Focus)`
   color: #4c80b0;
+`
+
+const FadeIn = styled.div`
+  @keyframes fadein {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`
+
+const Group1Transition = styled(FadeIn)`
+  animation: 1s fadein;
+`
+
+const Group2Transition = styled(FadeIn)`
+  animation: 1.5s fadein;
+`
+
+const Group3Transition = styled(FadeIn)`
+  animation: 1.6s fadein;
 `
 
 export default () => (
   <>
     <Body>
       <Intro>
-        Hi! <br /> My name is <br />
-        <Focus>Anthony Powell</Focus>,
-        <br /> but most people call me
-        <br /> <Focus>Tony</Focus>.
+        <Group1Transition>
+          <Focus>Hello! 👋</Focus>
+        </Group1Transition>
+        <Group2Transition>
+          My name is <br />
+          <Name>Anthony Powell</Name>
+          <br />
+        </Group2Transition>
+        <Group3Transition>
+          but most people call me
+          <br /> <Name>Tony</Name>
+        </Group3Transition>
       </Intro>
     </Body>
   </>
