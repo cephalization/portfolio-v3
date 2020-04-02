745a16864ca2b6628833655f781bc4ffc2724efd
Tony <powell.anthonyd@gmail.com>
2020-04-02 17:15:58 -0400

    Fix typo

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 
 import Github from "../icons/github.svg"
-import Twitter from "../icons/Twitter.svg"
+import Twitter from "../icons/twitter.svg"
 import LinkedIn from "../icons/linkedin.svg"
 
 const Container = styled.div`
